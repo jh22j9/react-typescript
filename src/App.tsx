83b26ce18e529726e4ea5e5f676d2414242f1739
jheeju <jheeju.dev@gmail.com>
2022-02-15 22:39:09 +0900

    Typing Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Greet } from "./components/Greet";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Greet name="Vishwas" />
+    </div>
+  );
 }
 
 export default App;
