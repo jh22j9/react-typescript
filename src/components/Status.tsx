7af52f67b817706718a12c7d19b3555d061fcad3
jheeju <jheeju.dev@gmail.com>
2022-02-16 00:02:45 +0900

    Advanced Props

---
+++ src/components/Status.tsx
@@ -0,0 +1,20 @@
+type StatusProps = {
+  // status: string
+  status: "loading" | "success" | "error";
+};
+
+export const Status = (props: StatusProps) => {
+  let message;
+  if (props.status === "loading") {
+    message = "Loading...";
+  } else if (props.status === "success") {
+    message = "Data fetched successfully";
+  } else if (props.status === "error") {
+    message = "Error fetching data";
+  }
+  return (
+    <div>
+      <h2>{message}</h2>
+    </div>
+  );
+};
