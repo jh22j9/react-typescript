e65d2a0a813b36843582796b8dda49ce3f019ad9
jheeju <jheeju.dev@gmail.com>
2022-02-16 22:18:04 +0900

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
+import { Container } from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => console.log("Button clicked", event, id)}
-      />
-      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
