d8c3973ff3474b05fd1ea7043dd8fd385bf91082
jheeju <jheeju.dev@gmail.com>
2022-02-20 23:48:58 +0900

    useContext Future Value

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ThemeContextProvider } from "./components/context/ThemeContext";
-import { Box } from "./components/context/Box";
+import { UserContextProvider } from "./components/context/UserContext";
+import { User } from "./components/context/User";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Box />
-      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
---
+++ src/components/context/User.tsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+export const User = () => {
+  const userContext = useContext(UserContext);
+  const handleLogin = () => {
+    if (userContext) {
+      userContext.setUser({
+        name: "Vishwas",
+        email: "vishwas@example.com",
+      });
+    }
+  };
+  const handleLogout = () => {
+    if (userContext) {
+      userContext.setUser(null);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext?.user?.name}</div>
+      <div>User email is {userContext?.user?.email}</div>
+    </div>
+  );
+};
